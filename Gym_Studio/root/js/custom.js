f90e031a0aa28be8c381a0807a1a62a625e75059
MrSp0ck97 <haziq.2201@gmail.com>
2019-09-16 03:00:30 +0800

    initial commit of files

---
+++ Gym_Studio/root/js/custom.js
@@ -0,0 +1,12 @@
+/*========================================
+                Slider
+==========================================*/
+$(function () {
+    $("#slider-content").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
