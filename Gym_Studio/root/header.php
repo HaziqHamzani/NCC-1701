6ff03b46518dd52f6eb978bc43bf4411c6734697
MrSp0ck97 <haziq.2201@gmail.com>
2019-09-17 00:54:36 +0800

    Gym_Studio initial commit

---
+++ Gym_Studio/root/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>MNT Gym Studio</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Maven+Pro:400,500,700&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/font-awesone/css/fontawesome-all.min.css">
+
+        <link rel="shortcut icon" href="img/favicon.ico">
+
+        <link rel="stylesheet" href="css/owlcarousel/owl.carousel.min.css">
+
+        <link rel="stylesheet" href="css/owlcarousel/owl.theme.default.min.css">
+
+        <link rel="stylesheet" href="css/style.css">
+
+        <link rel="stylesheet" href="css/responsive.css">
+    </head>
+
+    <body>
+        <!--<div id="navbar">
+            <div id="logo">
+                <h2>MNT Gym Studio</h2>
+            </div>
+            <div id="links">
+                <a href="">CLASSES</a>
+                <a href="">TRAINERS</a>
+                <a href="">GALLERY</a>
+                <a href="">LOGIN</a>
+                <a href="">SIGNUP</a>
+            </div>
+        </div>-->
+
+        <div class="nav-container">
+            <nav class="nav-checkbox">
+                <a href="#" id="logo"><h2>MNT Gym Studio</h2></a>
+                <input id="tab-nav" type="checkbox" class="tab-nav">
+                <label for="tab-nav" class="tab-nav-label">Menu</label>
+                <ul class="tab-content">
+                    <li><a href="">CLASSES</a></li>
+                    <li><a href="">TRAINER</a></li>
+                    <li><a href="">GALLERY</a></li>
+                    <li><a href="">LOGIN</a></li>
+                    <li><a href="">SIGNUP</a></li>
+                </ul>
+            </nav>
+        </div>
+
